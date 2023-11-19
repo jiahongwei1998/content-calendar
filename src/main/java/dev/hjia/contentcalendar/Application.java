bcbb46fa537bcc98d80b1e397c70322da4a5302e
David Jia <105570809@qq.com>
2023-11-19 22:57:18 +0800

    initial comit

---
+++ src/main/java/dev/hjia/contentcalendar/Application.java
@@ -0,0 +1,16 @@
+package dev.hjia.contentcalendar;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    var context = SpringApplication.run(Application.class, args);
+    Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+  }
+
+}
---
+++ src/main/java/dev/hjia/contentcalendar/Message.java
@@ -0,0 +1,14 @@
+package dev.hjia.contentcalendar;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Message
+ */
+@Component
+public class Message {
+
+  public String getMessage() {
+    return "Hello World!";
+  }
+}
