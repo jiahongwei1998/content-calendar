92b23553ce0b685c773198cec75c07e308c352e0
David Jia <105570809@qq.com>
2023-11-20 23:04:42 +0800

    20231120-home

--- src/main/java/dev/hjia/contentcalendar/controller/ContentController.java
+++ src/main/java/dev/hjia/contentcalendar/controller/ContentController.java
@@ -2,11 +2,15 @@ package dev.hjia.contentcalendar.controller;
 
 import java.util.List;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -15,6 +19,7 @@ import dev.hjia.contentcalendar.repository.ContentCollectionRepository;
 
 @RestController
 @RequestMapping("/api/content")
+@CrossOrigin
 public class ContentController {
   private final ContentCollectionRepository repository;
 
@@ -36,9 +41,24 @@ public class ContentController {
         .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Content Not Found"));
   }
 
+  @ResponseStatus(HttpStatus.CREATED)
   @PostMapping("")
   public void create(@RequestBody Content content) {
     repository.save(content);
   }
 
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  @PutMapping("/{id}")
+  public void update(@RequestBody Content content, @PathVariable Integer id) {
+    if (!repository.existById(id)) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found!");
+    }
+    repository.save(content);
+  }
+
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  @DeleteMapping("/{id}")
+  public void delete(@PathVariable Integer id) {
+    repository.delete(id);
+  }
 }
--- src/main/java/dev/hjia/contentcalendar/repository/ContentCollectionRepository.java
+++ src/main/java/dev/hjia/contentcalendar/repository/ContentCollectionRepository.java
@@ -29,13 +29,22 @@ public class ContentCollectionRepository {
 
   @PostConstruct
   private void init() {
-    Content content = new Content(1, "My First Blog Post", "Myfirst blog post", Status.IDEA, Type.ARTILE,
+    Content content = new Content(1, "My First Blog Post", "My first blog post", Status.IDEA, Type.ARTILE,
         LocalDateTime.now(), null, "");
     contents.add(content);
   }
 
   public void save(Content content) {
+    contents.removeIf(c -> c.id().equals(content.id()));
     contents.add(content);
+  }
+
+  public boolean existById(Integer id) {
+    return contents.stream().filter(c -> c.id().equals(id)).count() == 1;
+  }
+
+  public void delete(Integer id) {
+    contents.removeIf(c -> c.id().equals(id));
   }
 
 }
